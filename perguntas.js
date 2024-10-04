7ae9bafdbb8e187edc17b70863f882763bbc0267
helloamendes06 <133047274+helloamendes06@users.noreply.github.com>
2024-10-04 10:57:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Agricultura',
+    'Qual é a principal agricultura?',
+    'Agricultura familiar'
+)
+
+criaCartao(
+    'Agropecuária',
+    'Quais os tipos de agropecuária?',
+    'Agropecuária extensiva       e        Agropecuária intensiva'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
